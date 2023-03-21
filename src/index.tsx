d8f317c22f1fadee78f6decf6ceedac9598bd246
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2023-03-21 09:59:33 +0400

    3

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "../src/components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,11 +11,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/mysitemovies">
+  <HashRouter basename="/mysitemovies">
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
